53f434777ded0fc6a6902b848089612807d64cd8
Alejandro <alejandro.rodriguez95@gmail.com>
2020-10-05 13:51:23 +0200

    finished challenge

--- challenge/javascript.js
+++ challenge/javascript.js
@@ -1,1 +1,32 @@
 var jsonData = JSON.parse(images);
+
+for(var i=0;i<jsonData.length;i++)
+{
+    $(".container").append(`<div id="${i}" class="img-container"><img id="img${i}"src='${jsonData[i].image}'></div>`);
+    $(`#${i}`).append(`<br><span>Relation: ${jsonData[i].relation}</span>`);
+    $(`#${i}`).append(`<br><span>Name: ${jsonData[i].name}</span>`);
+}
+
+for(var i=0;i<jsonData.length;i++)
+{
+    if(jsonData[i].relation == 'father')
+    {
+        $(`#img${i}`).css("border", "blue 2px solid");
+    } 
+    if(jsonData[i].relation == 'grandpa')
+    {
+        $(`#img${i}`).css("border", "yellow 2px solid");
+    } 
+    if(jsonData[i].relation == 'dog')
+    {
+        $(`#img${i}`).css("border", "red 2px solid");
+    } 
+    if(jsonData[i].relation == 'sister')
+    {
+        $(`#img${i}`).css("border", "green 2px solid");
+    } 
+    
+}
+
+
+// `<img src="${jsonData[0].image}>"`
