0afd16a90cb93d7d188f34a34e3bbec1172c9f9e
Johannes Zarre <zarre.johannes@gmail.com>
2020-10-05 11:51:41 +0200

    add advanced

---
+++ advanced/adv.js
@@ -0,0 +1,21 @@
+$("a").attr("href", "www.codefactory.wien");
+
+$("code").css("color","red");
+
+$("ol > li").css("background-color","#2a7b90");
+
+$("input").val("Express your Opinion")
+
+$("img").attr("src","https://codefactory.wien/wp-content/themes/hestia/cf-images/big-jpg/1-frontend.jpg");
+
+$("blockquote").css({"background-color":"orange","font-style": "italic"});
+
+$(".gray").css("color","white");
+$("#logo").css("color","black");
+
+$(".date").css("display","none");
+
+
+$("ul:nth-child(2)").css("background-color","red");
+
+$("p:contains('afraid')").text("When someone says: 'I want a programming language in which I need only to say what I wish, and done', give him a lollipop.");
